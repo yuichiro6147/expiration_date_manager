590b754ffecaf7a48345fa762aa2029ebafe7cf8
Yuichiro Takeda <tak6147@gmail.com>
2020-12-23 21:57:26 +0900

    フッタ画面を作成

---
+++ lib/footer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatefulWidget {
+  const Footer();
+
+  @override
+  _Footer createState() => _Footer();
+}
+
+class _Footer extends State {
+  int _selectedIndex = 0;
+  final _bottomNavigationBarItems = <BottomNavigationBarItem>[];
+
+  // アイコン情報
+  static const _footerIcons = [Icons.calendar_today, Icons.list];
+
+  // アイコン文字列
+  static const _footerItemNames = ['カレンダー', '一覧'];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavigationBarItems.add(_updateActiveState(0));
+    for (var i = 1; i < _footerItemNames.length; i++) {
+      _bottomNavigationBarItems.add(_updateDeactiveState(i));
+    }
+  }
+
+  // インデックスのアイテムをアクティベートする
+  BottomNavigationBarItem _updateActiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black87,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        ));
+  }
+
+  // インデックスのアイテムをディアクティベートする
+  BottomNavigationBarItem _updateDeactiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black26,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black26,
+          ),
+        ));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] =
+          _updateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _updateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: _bottomNavigationBarItems,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    );
+  }
+}
